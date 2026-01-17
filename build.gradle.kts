f2508cdf80010f8957c8c7822bf412c56a57efa0
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2026-01-17 16:10:07 +0100

    fix: publish snapshots to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("multiplatform") version "2.0.21"
     kotlin("plugin.serialization") version "2.0.21"
     id("com.strumenta.antlr-kotlin") version "1.0.9"
-    id("com.vanniktech.maven.publish") version "0.30.0"
+    id("com.vanniktech.maven.publish") version "0.31.0"
     id("org.jetbrains.kotlinx.kover") version "0.9.4"
 }
 
